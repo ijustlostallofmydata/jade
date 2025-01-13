98733660e0fe7701b7fe9a2b9068eefca954d9fa
wc2890 <chenw4972@gmail.com>
2025-01-13 11:23:53 -0500

    Removed a file

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -10,15 +10,15 @@ function App() {
 
   return (
     <>
-      <div>
-      <NavigationBar></NavigationBar>
-      </div>
-        <p>
-          This is a paragraph. You can type stuff you want to see here.
-        </p>
-        <p>
-          This is used for another paragraph.
-        </p>
+	    <h1 id ="Heading">
+	      How h1 Looks like
+	    </h1>
+	    <p id ="Paragraph">
+	      This is a paragraph. You can type stuff you want to see here.
+	    </p>
+	    <p id = "Next">
+	      This is used for another paragraph.
+	    </p>
     </>
   )
 }
